1a95e46efb82c3d852d603392741ebac75c38583
Puneet Yadav <puneety259@gmail.com>
2024-07-31 18:47:35 +0530

    project name changed

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,11 @@ const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', // Allow requests from your React app
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
 
 app.post('/send', (req, res) => {
   const { name, email, message } = req.body;
--- Frontend/src/components/Project.jsx
+++ Frontend/src/components/Project.jsx
@@ -8,15 +8,15 @@ import { Pagination, Autoplay } from "swiper/modules";
 import project1 from "../assets/project1.png";
 import project2 from "../assets/project-2.jpg";
 import project3 from "../assets/project_3.png";
-import project4 from "../assets/project1.png";
+import project4 from "../assets/portfolio.png";
 
 const Project = () => {
 
   const projects = [
     {logo: project1, name:'My App', github: 'http://github.com/realpuneet/IMDB-CLONE'},
     {logo: project2, name:'Job Search', github: 'http://github.com/realpuneet/IMDB-Clone'},
-    {logo: project3, name:'Star Website', github: 'http://github.com/realpuneet/IMDB-Clone'},
-    {logo: project4, name:'PortFolio', github: 'http://github.com/realpuneet/IMDB-Clone'},
+    {logo: project3, name:'Space Tourism', github: 'http://github.com/realpuneet/IMDB-Clone'},
+    {logo: project4, name:'Portfolio', github: 'http://github.com/realpuneet/IMDB-Clone'},
   ]
 
   return (
@@ -28,11 +28,11 @@ const Project = () => {
         <p className="text-gray-400 mt-3 text-lg">My awesome works</p>
       </div>
       <br />
-      <div className="flex max-w-2xl px-5 mx-auto items-center relative">
-        <div className="lg:w-50 w-full">
+      <div className="flex max-w-2xl px-1 mx-auto items-center relative">
+        <div className="lg:w-full w-full">
           <Swiper
             slidesPerView={1.2}
-            spaceBetween={20}
+            spaceBetween={25}
             breakpoints={{
               768: {
                 slidesPerView: 3,
@@ -50,7 +50,7 @@ const Project = () => {
            {
             projects?.map((proj, i)=>(
               <SwiperSlide>
-              <div key={i} className="h-fit w-full p-4 bg-slate-700 rounded-xl">
+              <div key={i} className="h-fit w-full p-2 bg-slate-700 rounded-xl">
                 <img src={proj.logo} alt="Project 1" className="rounded-lg" />
                 <h3 className="text-xl my-4">{proj.name}</h3>
                 <div className="flex gap-3">
